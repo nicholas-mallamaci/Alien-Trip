54247c265c0caaa4eb5d1ae7f06171523c710aa1
Enrico2002 <enrico_damico@studenti.naba.it>
2022-07-06 00:21:32 +0200

    SoundCheck

--- Assets/Script/FirePowerUp.cs
+++ Assets/Script/FirePowerUp.cs
@@ -7,13 +7,18 @@ public class FirePowerUp : MonoBehaviour
 {
     public GameObject laser;
     public GameObject pickup;
+    public AudioSource audioData;
+ 
+
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collison)
     {
         if(collison.tag == "Laser")
-        {
+        { 
+            audioData.Play (); 
             laser.SetActive(true);
             Destroy(pickup);
+            
         }
     }
 }
